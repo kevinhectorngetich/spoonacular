006af0fc241b0dca499b9b6796ea010a33f38776
Kevinhector Ngetich <47977576+kevinhectorngetich@users.noreply.github.com>
2024-06-07 13:44:30 +0300

    Update recipe_service.dart

--- lib/services/recipe_service.dart
+++ lib/services/recipe_service.dart
@@ -8,7 +8,7 @@ class RecipeService {
 
   Future<List<Recipe>> fetchRecipes() async {
     try {
-      const apiKey = '97c75dcdd9f2473888bb2b0fb4300af5';
+      const apiKey = 'YOUR_API_KEY';
       final response = await dio.get(
           'https://api.spoonacular.com/recipes/random?number=10&apiKey=$apiKey');
       final recipesJson = response.data['recipes'] as List<dynamic>;
